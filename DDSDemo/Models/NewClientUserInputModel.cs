7e4506dbced39c40dfd4a37ef556636abcb6178c
Roark Acord <roark.acord@hotmail.com>
2017-07-13 15:14:38 -0400

    Add user for client functionality

---
+++ DDSDemo/Models/NewClientUserInputModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DDSDemo.Models
+{
+    public class NewClientUserInputModel
+    {
+        public int ID { get; set; }
+        public string Email { get; set; }
+
+    }
+}
---
+++ DDSDemo/Services/ClientAddUserService.cs
@@ -0,0 +1,36 @@
+﻿using DDSDemo.Models;
+using DDSDemoDAL;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using Microsoft.Owin;
+using System.Threading.Tasks;
+
+namespace DDSDemo.Services
+{
+    public class ClientAddUserService
+    {
+        public async Task<IdentityResult> AddUserClient(NewClientUserInputModel model, ApplicationUserManager UserManager)
+        {
+            var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+            var result = await UserManager.CreateAsync(user, "StaffFinder2017");
+            if (result.Succeeded)
+            {
+                await UserManager.AddToRoleAsync(user.Id, "Client");
+                await UserManager.AddClaimAsync(user.Id, new Claim("ClientID", model.ID.ToString()));
+
+                // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
+                // Send an email with this link
+                // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
+                // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
+                // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
+            }
+
+            return result;
+        }
+    }
+}
