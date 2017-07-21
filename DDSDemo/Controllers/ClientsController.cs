d425370402dd546278180e37a314f7ed02ff5196
Roark Acord <roark.acord@hotmail.com>
2017-07-21 10:50:55 -0400

    Client/Edit ActionResult was Async but did not use await

--- DDSDemo/Controllers/ClientsController.cs
+++ DDSDemo/Controllers/ClientsController.cs
@@ -150,7 +150,7 @@ namespace DDSDemo.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Edit([Bind(Include = "ID,CompanyName,EmployerID,EmployerName,Address1,Address2,City,State,Zip,Email,Phone")] Client client)
+        public ActionResult Edit([Bind(Include = "ID,CompanyName,EmployerID,EmployerName,Address1,Address2,City,State,Zip,Email,Phone")] Client client)
         {
             if (ModelState.IsValid)
             {
