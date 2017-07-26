799bc6b1c1fb507acedab7c6361c04fa1c34b784
Dallas Hartman <dh0014@mix.wvu.edu>
2017-07-26 13:39:08 -0400

    Admin sorting and searching

--- DDSDemo/Controllers/AdminController.cs
+++ DDSDemo/Controllers/AdminController.cs
@@ -64,11 +64,45 @@ namespace DDSDemo.Controllers
         private DDSContext dbb = new DDSContext();
 
         // GET: Admin
-        public ActionResult Index(int? page)
+        public ActionResult Index(string searchBy, string search, int? page, string sortBy)
         {
-            var users = UserManager.Users.ToList().AsQueryable().Where(u => u.Claims.Any(t => t.ClaimType == "Admin"));
+            ViewBag.CurrentPage = page;
+            var users = UserManager.Users.Where(u => u.Claims.Any(t => t.ClaimType == "Admin"));
 
-            return View(users.ToPagedList(page ?? 1, 10));
+            var data = users.AsQueryable();
+
+            ViewBag.SortFirstNameParameter = string.IsNullOrEmpty(sortBy) ? "FirstName Desc" : "";
+            ViewBag.SortLastNameParameter = sortBy == "LastName" ? "LastName Desc" : "LastName";
+
+            if (searchBy == "FirstName")
+            {
+                data = data.Where(x => x.FirstName.StartsWith(search) || search == null);
+            }
+            else
+            {
+                data = data.Where(x => x.LastName.StartsWith(search) || search == null);
+            }
+
+            switch (sortBy)
+            {
+                case "FirstName Desc":
+                    data = data.OrderByDescending(x => x.FirstName);
+                    break;
+                case "LastName":
+                    data = data.OrderBy(x => x.LastName);
+                    break;
+                case "LastName Desc":
+                    data = data.OrderByDescending(x => x.LastName);
+                    break;
+                default:
+                    data = data.OrderBy(x => x.FirstName);
+                    break;
+            }
+
+
+
+
+            return View(data.ToPagedList(page ?? 1, 10));
         }
         //// GET: Admin/Details/0bb0b0bb-0b0b-00bb-bb0b-00b000bb0000
         //public ActionResult Details(string id)
@@ -86,7 +120,7 @@ namespace DDSDemo.Controllers
         //}
 
         // GET: Admin/Create
-        public ActionResult Create(int? page)
+        public ActionResult Create(string searchBy, string search, int? page, string sortBy)
         {
             ViewBag.CurrentPage = page;
             ViewBag.EmailTaken = "";
@@ -98,7 +132,7 @@ namespace DDSDemo.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Create(int? page,[Bind(Include = "FirstName,LastName,Email,PhoneNumber")] NewAdminUserInputModel admin)
+        public async Task<ActionResult> Create(string searchBy, string search, int? page, string sortBy, [Bind(Include = "FirstName,LastName,Email,PhoneNumber")] NewAdminUserInputModel admin)
         {
             ViewBag.CurrentPage = page;
             if (ModelState.IsValid)
@@ -132,7 +166,7 @@ namespace DDSDemo.Controllers
 
 
         // GET: Admin/Edit/0bb0b0bb-0b0b-00bb-bb0b-00b000bb0000
-        public ActionResult Edit(string id, int? page)
+        public ActionResult Edit(string id, string searchBy, string search, int? page, string sortBy)
         {
             ViewBag.CurrentPage = page;
             ViewBag.EmailTaken = "";
@@ -154,7 +188,7 @@ namespace DDSDemo.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int? page,[Bind(Include = "Id,FirstName,LastName,Email,EmailConfirmed,PasswordHash,SecurityStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEndDateUtc,LockoutEnabled,AccessFailedCount,UserName")] ApplicationUser user)
+        public ActionResult Edit(string searchBy, string search, int? page, string sortBy, [Bind(Include = "Id,FirstName,LastName,Email,EmailConfirmed,PasswordHash,SecurityStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEndDateUtc,LockoutEnabled,AccessFailedCount,UserName")] ApplicationUser user)
         {
             ViewBag.CurrentPage = page;
             if (ModelState.IsValid)
@@ -188,7 +222,7 @@ namespace DDSDemo.Controllers
 
                         if (result.Succeeded)
                         {
-                            return RedirectToAction("Index", new { page = page});
+                            return RedirectToAction("Index", new { page = page, searchBy = Request.QueryString["searchBy"], search = Request.QueryString["search"], sortBy = Request.QueryString["sortBy"] });
                         }
                     }
                     else
@@ -209,7 +243,7 @@ namespace DDSDemo.Controllers
         }
 
         // GET: Admin/Delete/0bb0b0bb-0b0b-00bb-bb0b-00b000bb0000
-        public ActionResult Delete(String id, int? page)
+        public ActionResult Delete(String id, string searchBy, string search, int? page, string sortBy)
         {
             ViewBag.CurrentPage = page;
             if (id == null)
@@ -227,13 +261,13 @@ namespace DDSDemo.Controllers
         // POST: Admin/Delete/0bb0b0bb-0b0b-00bb-bb0b-00b000bb0000
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(string id, int? page)
+        public ActionResult DeleteConfirmed(string id, string searchBy, string search, int? page, string sortBy)
         {
             ViewBag.CurrentPage = page;
             ApplicationUser user = db.Users.Find(id);
             db.Users.Remove(user);
             db.SaveChanges();
-            return RedirectToAction("Index", new { page = page });
+            return RedirectToAction("Index", new { page = page, searchBy = Request.QueryString["searchBy"], search = Request.QueryString["search"], sortBy = Request.QueryString["sortBy"] });
         }
 
         protected override void Dispose(bool disposing)
